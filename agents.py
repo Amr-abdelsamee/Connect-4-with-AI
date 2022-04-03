b292b3c3327343a555e70a2f9b6890d5c76d548b
Nour Hesham Shaheen <nourheshamshaheen11@gmail.com>
2022-04-04 01:28:41 +0200

    Add function get_next_states in agent.py

--- agents.py
+++ agents.py
@@ -13,10 +13,10 @@ class MinMax:
         pass
 
     def maximize(self, state):
-        if math.log(self.current_depth, 7) == self.depth:
+        if math.log(self.current_depth, 7) - 1 == self.depth:
             return None, heu(state)
         max_child, max_utility = None, float('-inf')
-        children = self.get_next_states(state)
+        children = self.get_next_states(state, True)
         for child in children:
             _, utility = minimize(child)
             if utility > max_utility:
@@ -24,11 +24,11 @@ class MinMax:
         self.current_depth += 1
         return max_child, max_utility
 
-    def minimize(self,state):
-        if math.log(self.current_depth, 7) == self.depth:
+    def minimize(self, state):
+        if math.log(self.current_depth, 7) - 1 == self.depth:
             return None, heu(state)
         min_child, min_utility = None, float('inf')
-        children = self.get_next_states(state)
+        children = self.get_next_states(state, False)
         for child in children:
             _, utility = maximize(child)
             if utility < min_utility:
@@ -41,8 +41,23 @@ class MinMax:
         child, utility = self.maximize(state)
         return child
 
-    def get_next_states(self, state):
+    def get_next_states(state, turn):  # turn is True for AI, False for Human
+        if turn:
+            char = '2'
+        else:
+            char = '1'
         children = []
+        for i in range(7):
+            index = -1
+            child = ""
+            for character in state[::-1]:
+                if character == '0':
+                    index += 1
+                if character == '0' and index == i:
+                    child = char + child
+                else:
+                    child = character + child
+            children.append(child)
         return children
 
     def work(self):
