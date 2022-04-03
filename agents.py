80edaf224cf50d32091539fd492ac334f0d3de25
Nour Hesham Shaheen <nourheshamshaheen11@gmail.com>
2022-04-04 01:31:28 +0200

    Update depth logic in agents.py

--- agents.py
+++ agents.py
@@ -4,7 +4,6 @@ class MinMax:
     def __init__(self, depth):
         self.state = ''
         self.depth = depth
-        self.current_depth = 0
 
     def update(self, state):
         self.state = state
@@ -12,33 +11,30 @@ class MinMax:
     def heu(self, state):
         pass
 
-    def maximize(self, state):
-        if math.log(self.current_depth, 7) - 1 == self.depth:
+    def maximize(self, state, d):
+        if d == self.depth:
             return None, heu(state)
         max_child, max_utility = None, float('-inf')
         children = self.get_next_states(state, True)
         for child in children:
-            _, utility = minimize(child)
+            _, utility = minimize(child, d + 1)
             if utility > max_utility:
                 max_child, max_utility = child, utility
-        self.current_depth += 1
         return max_child, max_utility
 
-    def minimize(self, state):
-        if math.log(self.current_depth, 7) - 1 == self.depth:
+    def minimize(self, state, d):
+        if d == self.depth:
             return None, heu(state)
         min_child, min_utility = None, float('inf')
         children = self.get_next_states(state, False)
         for child in children:
-            _, utility = maximize(child)
+            _, utility = maximize(child, d + 1)
             if utility < min_utility:
                 min_child, min_utility = child, utility
-        self.current_depth += 1
         return min_child, min_utility
 
     def decision(self):
-        self.current_depth = 0
-        child, utility = self.maximize(state)
+        child, utility = self.maximize(state, 0)
         return child
 
     def get_next_states(state, turn):  # turn is True for AI, False for Human
