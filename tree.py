5fd717a9172359376db9cd7d10718ae9b37a5e46
Amr Abdelsamee <67904417+Amr-abdelsamee@users.noreply.github.com>
2022-04-09 04:46:54 +0200

    last ver

--- tree.py
+++ tree.py
@@ -1,6 +1,5 @@
 from graphviz import Digraph
 
-
 class Tree:
     def __init__(self, states, num_col, num_row):
         self.graph = Digraph()
@@ -11,9 +10,11 @@ class Tree:
         self.num_row = num_row
         self.create_tree()
 
+
     def create_tree(self):
         tree_len = len(self.tree)
         self.graph.attr('node', shape='box')
+        self.graph.attr( pad="0.5", ranksep="1.5")
         for i in range(tree_len):
             self.graph.node(str(self.tree[i][0]),
                             str(self.tree[i][0][4]) + '\n' + str(self.tree[i][0][3]) + '\n' + str(self.tree[i][0][2]))
@@ -22,8 +23,11 @@ class Tree:
                                 str(self.tree[i][1][j][4]) + '\n' + str(self.tree[i][1][j][3]) + '\n' + str(
                                     self.tree[i][1][j][2]))
                 self.graph.edge(str(self.tree[i][0]), str(self.tree[i][1][j]))
+        print("creating the tree is done")
+
 
     def save_tree(self, display):
         self.graph.format = self.extension
-        tree = self.graph.unflatten(stagger=3)
+        tree = self.graph.unflatten(stagger=2)
         tree.render(self.png_name, view=display)
+        print("rendering the tree is done")
